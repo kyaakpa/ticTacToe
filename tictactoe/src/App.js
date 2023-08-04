cd73b12f2ec930adee615a7d9b26c1adcf0c94d2
kyaakpa <kyaakpa@users.noreply.github.com>
2023-08-04 19:52:57 +1000

    .

---
+++ tictactoe/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const Square = () => {
+  return (
+    <div className="container">
+      <button className="square">X</button>
+    </div>
+  )
+}
+
+export default Square;
